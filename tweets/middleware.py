477de382cee85188dd88d79c148f7eba65d8a91c
Alt0b11 <thanksuniverse333@outlook.com>
2024-08-18 19:20:20 -0300

    fix: allows some pages without authentication

--- tweets/middleware.py
+++ tweets/middleware.py
@@ -5,10 +5,10 @@ class RequireLoginMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if request.path in ['/hello/', '/update_server/', 'api/endpoints/']:
+            return self.get_response(request)
         if not request.user.is_authenticated and request.path not in ['/accounts/login/', '/accounts/register/']:
             return redirect('register')
         if request.user.is_authenticated and request.path in ['/accounts/login/', '/accounts/register/']:
             return redirect('home')
-        if request.path in ['/hello/', '/update_server/', 'api/endpoints/']:
-            return self.get_response(request)
         return self.get_response(request)
