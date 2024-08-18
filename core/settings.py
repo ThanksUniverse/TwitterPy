385c80796511f6c36f797111a793aeff393bc254
Alt0b11 <thanksuniverse333@outlook.com>
2024-08-18 19:46:38 -0300

    feat: configured new database

--- core/settings.py
+++ core/settings.py
@@ -84,12 +84,15 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbdev',
+        'USER': 'alt0b011',
+        'PASSWORD': 'IncreadibleStrongPassword12321!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
