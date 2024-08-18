a2a47bb9cc1b1a0333efd25800904d0a7a12e17e
Alt0b11 <thanksuniverse333@outlook.com>
2024-08-18 19:21:28 -0300

    fix: allows some pages without authentication

--- tweets/views/pythonanywhere_view.py
+++ tweets/views/pythonanywhere_view.py
@@ -13,7 +13,7 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo('/home/drsantos20/bookstore')
+        repo = git.Repo('/home/alt0b011/TwitterPy')
         origin = repo.remotes.origin
 
         origin.pull()
